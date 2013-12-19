09bb0b37ecb0c404d4154eebcf96f792af0853b9
dvlprliu <dvlprliu@gmail.com>
2013-12-19 14:18:04 +0800

    Initial Commit

---
+++ NTLimitationInputView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NTLimitationInputView
+//
+//  Created by FFF on 13-12-19.
+//  Copyright (c) 2013年 Liu Zhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
